95cfa3220d6d18f0347e5d9cc4b55f05a98cf525
Seth Miller <seth.paul.miller@gmail.com>
2017-11-04 23:08:47 -0400

    Update classBox.java
    Added comments to code.

--- classBox.java
+++ classBox.java
@@ -2,7 +2,6 @@ package application;
 
 import javafx.geometry.Insets;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -14,33 +13,41 @@ public class classBox {
 	double frameY = 400;
 	double orgSceneX, orgSceneY;
 	
+	//Constructor to initialize object
 	public classBox() 
 	{
 		
 	}
 	
+	/**
+	 * 
+	 * @returns a newly created VBox to be set as
+	 * 			a new class box, its location and 
+	 * 			listens for a mouse click and drag
+	 * 			to move its location.
+	 */
 	public VBox createBox()
 	{
 		VBox group = new VBox(8);
 		TextArea text1 = new TextArea();
 		text1.setPrefHeight(50);
 		text1.setPrefWidth(250);
+		text1.setWrapText(true);
 		TextArea text2 = new TextArea();
 		text2.setPrefHeight(100);
 		text2.setPrefWidth(250);
+		text2.setWrapText(true);
 		TextArea text3 = new TextArea();
 		text3.setPrefHeight(100);
 		text3.setPrefWidth(250);
+		text3.setWrapText(true);
 		group.setLayoutX(frameX);
 		group.setLayoutY(frameY);
 		group.setPadding(new Insets(50, 5, 5, 5));
 		group.setBackground(new Background(new BackgroundFill(Color.web("#C0C0C0"), CornerRadii.EMPTY, Insets.EMPTY)));
 		group.getChildren().addAll(text1, text2, text3);
 		
-		
-	
-	
-	   group.setOnMousePressed((t) -> 
+		group.setOnMousePressed((t) -> 
 	    {
 	      orgSceneX = t.getSceneX();
 	      orgSceneY = t.getSceneY();
